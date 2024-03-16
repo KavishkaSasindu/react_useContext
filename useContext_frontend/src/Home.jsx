c40a6e230b7c8568c47163f2c62d1244c51ce7de
KavishkaSasindu <sasindu0215@gmail.com>
2024-03-17 00:49:14 +0530

    updated

--- useContext_frontend/src/Home.jsx
+++ useContext_frontend/src/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <div>Home page</div>
     </div>
   );
--- useContext_frontend/src/components/Navbar.jsx
+++ useContext_frontend/src/components/Navbar.jsx
@@ -1,10 +1,40 @@
 import React from "react";
+import { useState } from "react";
+import "../App.css";
 
 const Navbar = () => {
+  const [logIn, setLogIn] = useState(true);
   return (
     <div>
       <div className="navbar-inner">
-        <div className="navbar-outer"></div>
+        {logIn && (
+          <div className="navbar-outer">
+            <div className="logo">
+              <h4>Logo</h4>
+            </div>
+            <div className="list-items">
+              <ul>
+                <li>contact</li>
+                <li>admin</li>
+                <li>logout</li>
+              </ul>
+            </div>
+          </div>
+        )}
+        {!logIn && (
+          <div className="navbar-outer">
+            <div className="logo">
+              <h4>Logo</h4>
+            </div>
+            <div className="list-items">
+              <ul>
+                <li>contact</li>
+                <li>admin</li>
+                <li>signIn</li>
+              </ul>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
